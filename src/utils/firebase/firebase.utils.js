bfefcf629bd6e4a1d962c85e6f6749d85bdcd451
MonLes <73730090+Monles@users.noreply.github.com>
2022-04-27 08:21:02 +0000

    Update firebase.utils.js

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -21,12 +21,7 @@ import {
 } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+ YOUR_FIREBASE
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
