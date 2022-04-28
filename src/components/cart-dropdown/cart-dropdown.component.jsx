afa47e724f3cf3b185cce0052d3b4d49dd7d89b3
Monles <iceueb@gmail.com>
2022-04-28 02:03:22 +0000

    some test

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,23 +1,26 @@
-import { useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
-import { CartContext } from '../../contexts/cart.context';
+import { CartContext } from "../../contexts/cart.context";
+import { CategoriesContext } from "../../contexts/categories.context";
 
-import Button from '../button/button.component';
-import CartItem from '../cart-item/cart-item.component';
+import Button from "../button/button.component";
+import CartItem from "../cart-item/cart-item.component";
 
 import {
   CartDropdownContainer,
   EmptyMessage,
   CartItems,
-} from './cart-dropdown.styles';
+} from "./cart-dropdown.styles";
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
+  const { categoriesMap } = useContext(CategoriesContext);
+  console.log(categoriesMap);
   const navigate = useNavigate();
 
   const goToCheckoutHandler = () => {
-    navigate('/checkout');
+    navigate("/checkout");
   };
 
   return (
